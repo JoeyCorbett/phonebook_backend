ac1a95db06ae7ebeb8f8285b848fd53ab4d1e35e
JoeyCorbett <corbettjoey9@gmail.com>
2025-01-14 04:43:00 -0500

    Complete 3.19

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const errorHandler = (error, req, res, next) => {
 
   if (error.name === 'CastError') {
     return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
   }
 
   next(error)
@@ -57,7 +59,7 @@ app.get("/info", (req, res) => {
   })
 })
 
-app.post("/api/persons", (req, res) => {
+app.post("/api/persons", (req, res, next) => {
   const body = req.body
 
   if (!body.name || !body.number) {
@@ -74,17 +76,17 @@ app.post("/api/persons", (req, res) => {
   person.save().then((savedPerson) => {
     res.json(savedPerson)
   })
+  .catch(error => next(error))
 })
 
-app.put("/api/persons/:id", (req, res) => {
-  const body = req.body
+app.put("/api/persons/:id", (req, res, next) => {
+  const { name, number } = req.body
 
-  const person = {
-    name: body.name,
-    number: body.number,
-  }
-
-  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+  Person.findByIdAndUpdate(
+    req.params.id,
+    { name, number },
+    { new: true , runValidators: true, context: 'query' }
+  )
     .then(updatedPerson => {
       res.json(updatedPerson)
     })
