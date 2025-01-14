b33bbf800912f4b30d8f19dd2efd4a6646884999
JoeyCorbett <corbettjoey9@gmail.com>
2025-01-13 19:05:50 -0500

    Complete 3.13: fetch data from db

--- index.js
+++ index.js
@@ -1,41 +1,31 @@
 const express = require("express");
-const morgan = require('morgan')
-const cors = require('cors')
-
 const app = express();
-app.use(cors())
-app.use(express.static('dist'))
+require("dotenv").config();
 
-morgan.token('body', function(req, res) { return JSON.stringify(req.body) })
+const Person = require("./models/person");
+
+let persons = [];
+
+app.use(express.static("dist"));
+
+const morgan = require("morgan");
+const cors = require("cors");
+
+app.use(cors());
+
+morgan.token("body", function (req, res) {
+  return JSON.stringify(req.body);
+});
 
 app.use(express.json());
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 app.get("/api/persons", (req, res) => {
-  res.json(persons);
+  Person.find({}).then((people) => {
+    res.json(people);
+  });
 });
 
 app.get("/api/persons/:id", (req, res) => {
@@ -85,7 +75,7 @@ app.delete("/api/persons/:id", (req, res) => {
   res.status(204).end();
 });
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
